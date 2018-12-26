37fb7b12b27e359763c4ee0f48c344591f8d23ae
日野恵太 <keitahino0415@gmail.com>
2018-12-26 12:09:20 +0900

    add:helpメソッド追加

--- app/lib/command.rb
+++ app/lib/command.rb
@@ -6,4 +6,26 @@ class Command
         as_user: false
     }
   end
+  def self.help(channel_id)
+    help_text = <<-EOS
+      スラッシュコマンド一覧
+      /task_add タスク名 due: 21:00
+        →タスクを追加する。dueオプションをつけると期限を設定することができる。
+      /task_remove タスク名
+        →タスクを削除する。
+      /task_complete タスク名
+        →タスクのステータスを「完了」にする。
+      /task_show タスク名
+        →タスクの一覧を表示する。これも日程の範囲をオプションで受け取れるようにするといいかも。
+      /task_report
+        →完了したタスクなどから、日報を作成する。
+      /task_help
+        →各種コマンドの説明を表示する。
+    EOS
+    {
+        channel:channel_id,
+        text: help_text,
+        as_user: false
+    }
+  end
 end
