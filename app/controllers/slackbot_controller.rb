2688db1d9c4bbaaad30514b80dafa89f3d5bf281
日野恵太 <keitahino0415@gmail.com>
2018-12-26 13:30:42 +0900

    add:addアクション追加

--- app/controllers/slackbot_controller.rb
+++ app/controllers/slackbot_controller.rb
@@ -2,7 +2,14 @@ require 'slack-ruby-client'
 require 'slack-ruby-bot'
 
 class SlackbotController < ApplicationController
-  before_action :slack_init, only: [:report, :help]
+  before_action :slack_init, only: [:report, :help, :add]
+  def add
+    t = Task.new(
+      user_id:params['user_id'],
+      task_name:params['text']
+    )
+    t.save
+  end
   def report
     message = Command.report(params['channel_id'])
     @client.chat_postMessage(message)
