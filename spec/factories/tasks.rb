f5b05994128ea03ebdd9843ad31ee38488ebdf25
日野恵太 <keitahino0415@gmail.com>
2018-12-26 16:10:46 +0900

    refactor:factoryを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    user_id { "MyString" }
-    task_name { "MyString" }
+    user_id { "C0XXXX" }
+    task_name { "test" }
     completed { false }
     due_date { "2018-12-26 11:24:57" }
   end
