beff14d784308fee8314d44e27236fcdde5dc557
日野恵太 <keitahino0415@gmail.com>
2018-12-27 18:38:51 +0900

    refactor:factoryのuser_idを変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    user_id { "C0XXXX" }
+    user_id { "U999999" }
     task_name { "test" }
     completed { false }
     due_date { "2018-12-26 11:24:57" }
