337f28143d0a32576e9349143698a44576c4860c
日野恵太 <keitahino0415@gmail.com>
2018-12-28 14:31:33 +0900

    add:completeアクションのテストケース追加

--- spec/controllers/slackbot_controller_spec.rb
+++ spec/controllers/slackbot_controller_spec.rb
@@ -7,36 +7,57 @@ RSpec.describe SlackbotController, type: :controller do
 
   describe '#add' do
     it 'is task add correct' do
-      VCR.use_cassette("controller/add/post_show_success") do
+      VCR.use_cassette("controller/add/post_success") do
         post :add,params: {
           token:        'token',
           team_id:      'team_id',
           team_domain:  'team_domain',
-          channel_id:   'CEN70LSTT',
+          channel_id:   'test',
           channel_name: 'channel_name',
           user_id:      'C0XXXX',
           user_name:    'user_name',
           command:      '/task_add',
-          text:         'addコマンド作成',
+          text:         'test',
           response_url: 'https://example.com'
         }
       end
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe '#complete' do
+    it 'is task comcomplete correct' do
+      VCR.use_cassette("controller/complete/post_success") do
+        post :complete,params: {
+          token:        'token',
+          team_id:      'team_id',
+          team_domain:  'team_domain',
+          channel_id:   'test',
+          channel_name: 'channel_name',
+          user_id:      'C0XXXX',
+          user_name:    'user_name',
+          command:      '/task_complete',
+          text:         'test',
+          response_url: 'https://example.com'
+        }
+      end
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe '#show' do
     it 'is task show correct' do
-      VCR.use_cassette("controller/show/post_show_success") do
+      VCR.use_cassette("controller/show/post_success") do
         post :show,params: {
           token:        'token',
           team_id:      'team_id',
           team_domain:  'team_domain',
-          channel_id:   'CEN70LSTT',
+          channel_id:   'test',
           channel_name: 'channel_name',
           user_id:      'C0XXXX',
           user_name:    'user_name',
           command:      '/task_add',
-          text:         'addコマンド作成',
+          text:         'test',
           response_url: 'https://example.com'
         }
       end
