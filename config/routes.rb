5671d8321aec87b7cec7b4038970874a0c4975df
日野恵太 <keitahino0415@gmail.com>
2018-12-28 14:30:26 +0900

    add:completeルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/add' => 'slackbot#add'
+  post '/complete' => 'slackbot#complete'
   post '/show' => 'slackbot#show'
   post '/report' => 'slackbot#report'
   post '/help' => 'slackbot#help'
