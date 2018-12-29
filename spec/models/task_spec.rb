be47b760c5ed7b7967c965a6f4babee969b7a2e3
日野恵太 <keitahino0415@gmail.com>
2018-12-29 13:38:24 +0900

    add:today_create_taskメソッドなどのテストケースを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#today_create_task' do
+    it 'return create task list' do
+      today = Time.now.midnight
+      FactoryBot.create(:task,created_at:today)
+      expect(Task.today_create_task.count).to eq(1)
+    end
+  end
+
+  describe '#today_completed_task' do
+    it 'return complete task list' do
+      today = Time.now.midnight
+      FactoryBot.create(:task,updated_at:today,completed:true)
+      expect(Task.today_completed_task.count).to eq(1)
+    end
+  end
+
+  describe '#incomplete_task' do
+    it 'return incomplete task list' do
+      today = Time.now.midnight
+      FactoryBot.create(:task,created_at:today,completed:false)
+      expect(Task.incomplete_task.count).to eq(1)
+    end
+  end
 end
