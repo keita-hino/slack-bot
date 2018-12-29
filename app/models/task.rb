7cd5fb5d46da63a5dbc89ac044eb08341df44545
日野恵太 <keitahino0415@gmail.com>
2018-12-29 13:37:59 +0900

    add:today_create_taskメソッドなどを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
 class Task < ApplicationRecord
-  
+  def self.today_create_task
+    Task.where(created_at: Time.now.midnight..(Time.now.midnight + 1.day - 1))
+  end
+
+  def self.today_completed_task
+    Task.where(completed:true).where(updated_at: Time.now.midnight..(Time.now.midnight + 1.day - 1))
+  end
+
+  def self.incomplete_task
+    Task.where(completed:false)
+  end
+
 end
