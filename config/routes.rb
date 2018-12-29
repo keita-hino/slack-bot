95a1ade6a8f702079818ef1e2b53695189f19c16
日野恵太 <keitahino0415@gmail.com>
2018-12-29 10:34:44 +0900

    add:deleteアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post '/add' => 'slackbot#add'
   post '/complete' => 'slackbot#complete'
+  post '/delete' => 'slackbot#delete'
   post '/show' => 'slackbot#show'
   post '/report' => 'slackbot#report'
   post '/help' => 'slackbot#help'
