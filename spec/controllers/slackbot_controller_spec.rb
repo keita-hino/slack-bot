1d4f9391125d455aebf58b7af58c75fe4616fb68
日野恵太 <keitahino0415@gmail.com>
2018-12-29 13:01:55 +0900

    add:reportアクションのテストケース追加

--- spec/controllers/slackbot_controller_spec.rb
+++ spec/controllers/slackbot_controller_spec.rb
@@ -45,10 +45,30 @@ RSpec.describe SlackbotController, type: :controller do
     end
   end
 
+  describe '#report' do
+    it 'is task report correct' do
+      VCR.use_cassette("controller/report/post_success") do
+        post :report,params: {
+          token:        'token',
+          team_id:      'team_id',
+          team_domain:  'team_domain',
+          channel_id:   'test',
+          channel_name: 'channel_name',
+          user_id:      'C0XXXX',
+          user_name:    'user_name',
+          command:      '/task_complete',
+          text:         'test',
+          response_url: 'https://example.com'
+        }
+      end
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe '#delete' do
     it 'is task delete correct' do
       VCR.use_cassette("controller/delete/post_success") do
-        post :show,params: {
+        post :delete,params: {
           token:        'token',
           team_id:      'team_id',
           team_domain:  'team_domain',
