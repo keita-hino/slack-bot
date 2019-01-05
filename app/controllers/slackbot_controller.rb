4ecd142cb8e68e999b400e5638e5b2e91c424609
日野恵太 <keitahino0415@gmail.com>
2019-01-05 17:10:00 +0900

    add:modifyアクションを追加

--- app/controllers/slackbot_controller.rb
+++ app/controllers/slackbot_controller.rb
@@ -2,7 +2,7 @@ require 'slack-ruby-client'
 require 'slack-ruby-bot'
 
 class SlackbotController < ApplicationController
-  before_action :slack_init, only: [:report, :help, :add, :show, :complete, :delete]
+  before_action :slack_init, only: [:report, :help, :add, :show, :complete, :delete, :modify]
   def add
     message = Message.add_message(params['text'],params['channel_id'],params['user_id'])
     @client.chat_postMessage(message)
@@ -10,6 +10,11 @@ class SlackbotController < ApplicationController
 
   def complete
     message = Message.complete_message(params['text'],params['channel_id'])
+    @client.chat_postMessage(message)
+  end
+
+  def modify
+    message = Message.modify_message(params['text'],params['channel_id'])
     @client.chat_postMessage(message)
   end
 
