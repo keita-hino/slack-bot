fd0f12145fb1426d9c504639e3c73dff18b46a0f
日野恵太 <keitahino0415@gmail.com>
2019-02-16 10:23:41 +0900

    add:deleted_atカラム追加

---
+++ db/migrate/20190216001344_add_deletedat_to_task.rb
@@ -0,0 +1,6 @@
+class AddDeletedatToTask < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :deleted_at, :datetime
+    add_index :tasks, :deleted_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_023403) do
+ActiveRecord::Schema.define(version: 2019_02_16_001344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_01_05_023403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "started", default: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_tasks_on_deleted_at"
   end
 
 end
