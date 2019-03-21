93733dfcf9ceb773870dc9ab8147befabcd9876e
日野恵太 <keitahino0415@gmail.com>
2019-03-21 11:47:54 +0900

    modify:タスクの並びを降順に変更

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,5 +1,5 @@
 class TaskController < ApplicationController
   def index
-    @tasks = Task.order(:id).page(params[:page]).per(30)
+    @tasks = Task.order(updated_at: "desc").page(params[:page]).per(30)
   end
 end
