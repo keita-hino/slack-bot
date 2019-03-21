71db751522bf31a621925e904c85c1f8a06a9d04
日野恵太 <keitahino0415@gmail.com>
2019-03-21 09:49:52 +0900

    modify:apiモードを個別に設定

--- app/controllers/slackbot_controller.rb
+++ app/controllers/slackbot_controller.rb
@@ -1,7 +1,7 @@
 require 'slack-ruby-client'
 require 'slack-ruby-bot'
 
-class SlackbotController < ApplicationController
+class SlackbotController < ActionController::API
   before_action :slack_init, only: [:report, :help, :add, :show, :complete, :delete, :modify]
   def add
     message = Message.add_message(params['text'],params['channel_id'],params['user_id'])
