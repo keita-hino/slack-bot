2b297f0fbad57ac28256df0c35518e2faf4559fc
日野恵太 <keitahino0415@gmail.com>
2019-03-23 10:19:20 +0900

    add:ページネーションを日本語化

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Slackbot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.middleware.use ActionDispatch::Flash
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
